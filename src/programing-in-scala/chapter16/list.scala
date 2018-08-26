21cebeb686d71bd658e59879115bcdc95ee05f49
Ryo Matsumoto <ryo.mats302@gmail.com>
2018-08-26 12:00:23 +0900

    [add] Programming in Scala chapter 16

---
+++ src/programing-in-scala/chapter16/list.scala
@@ -0,0 +1,12 @@
+val fruit = List("apple", "orange", "banana")
+val nums = List(1,2,3,4)
+val diag3 = 
+  List(
+    List(1,0,0),
+    List(0,1,0),
+    List(0,0,1)
+  )
+val empty = List()
+
+
+val List(a, b, c) = fruit
