c722ea0a28da396fe563d3c3263b4e7eb7f22ea7
Ryo Matsumoto <ryo.mats302@gmail.com>
2018-08-28 17:15:43 +0900

    [add] programming in scala chapter 18 forward

---
+++ src/programing-in-scala/chapter18/sample.scala
@@ -0,0 +1,54 @@
+// ミュータブルオブジェクト
+
+val cs = List('a', 'b', 'c')
+
+// 純粋関数型オブジェクトとミュータブルオブジェクトの違い
+// 前者は、純粋な引数のみによって出力が一意に決定する。後者はそうではない。
+
+class BankAccount {
+  private var bal: Int = 0
+  def balance: Int = bal
+  def deposit(amount: Int) {
+    require(amount > 0)
+    bal += amount
+  }
+
+  def withdraw(amount: Int): Boolean = 
+    if(amount > bal) false
+    else {
+      bal -= amount
+      true
+    }
+}
+
+val account = new BankAccount
+account deposit 100
+account withdraw 80
+account withdraw 80
+
+
+class Time {
+  private[this] var h = 12
+  private[this] var m = 0
+  def hour: Int = h
+  def hour_= (x: Int) = {
+    require(0 <= x && x < 24)
+    h = x
+  }
+  def minute: Int = m
+  def minute_= (x: Int) = {
+    require(0 <= x && x < 60)
+    m = x
+  }
+}
+
+class Thermometer {
+  var celsius: Float = _
+  def fahrenheit = celsius * 9 / 5 + 32
+  def fahrenheit_= (f: Float) = {
+    celsius = (f - 32) * 5 / 9
+  }
+  override def toString = fahrenheit + "F/" + celsius + "C"
+}
+
+val t = new Thermometer
