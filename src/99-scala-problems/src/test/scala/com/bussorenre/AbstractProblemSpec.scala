0c0ab1528ce6486c5b0c4221e0655f5233d78f2c
Ryo Matsumoto <ryo.mats302@gmail.com>
2018-10-11 16:09:21 +0900

    99-scala-problems add Problem 1

---
+++ src/99-scala-problems/src/test/scala/com/bussorenre/AbstractProblemSpec.scala
@@ -0,0 +1,7 @@
+package com.bussorenre
+
+import org.scalatest.{MustMatchers, WordSpec}
+
+abstract class AbstractProblemSpec extends WordSpec with MustMatchers {
+
+}
---
+++ src/99-scala-problems/src/test/scala/com/bussorenre/s99/P01Spec.scala
@@ -0,0 +1,19 @@
+package com.bussorenre.s99
+
+import com.bussorenre.AbstractProblemSpec
+
+class P01Spec extends AbstractProblemSpec {
+  val sut = P01
+
+  "last" should {
+    "リストの最後の値を返す" in {
+      val args = List(1, 1, 2, 3, 5, 8)
+      sut.last(args) must be(8)
+    }
+
+    "空リストが渡された時、 例外を返す" in {
+      val args = List()
+      an [Throwable] must be thrownBy(sut.last(args))
+    }
+  }
+}
