fde7a9d6412a92e0abb399dd0ad3f753f86dc7f8
Ryo Matsumoto <ryo.mats302@gmail.com>
2019-10-03 20:14:05 +0900

    [s99] Problem 16 drop

---
+++ scala-study/modules/99-scala-problems/src/main/scala/com/bussorenre/s99/lists/P16.scala
@@ -0,0 +1,29 @@
+package com.bussorenre.s99.lists
+
+/**
+  * Drop every Nth element from a list.
+  */
+object P16 {
+  def drop[A](n: Int, list: List[A]): List[A] = {
+    def innerDrop[A](i: Int, rest: List[A]): List[A] = rest match {
+      case Nil => Nil
+      case x :: xs => i match {
+        case 1 => innerDrop(n, xs)
+        case i if i > 1 => x :: innerDrop(i - 1, xs)
+        case _ => sys.error("drop: n could not be below 0")
+      }
+    }
+    innerDrop(n, list)
+  }
+
+  // write more simple
+  def drop2[_](n: Int, list: List[_]): List[_] = {
+    def innerDrop(m: Int, list: List[_]): List[_] = (m, list) match {
+      case (1, _ :: xs ) => innerDrop(n, xs)
+      case (_,  x :: xs ) => x :: innerDrop(m - 1, xs)
+      case (_, _) => Nil
+    }
+    if(n > 0) innerDrop(n, list)
+    else sys.error("drop: n could not be below 0")
+  }
+}
---
+++ scala-study/modules/99-scala-problems/src/test/scala/com/bussorenre/s99/lists/P16Spec.scala
@@ -0,0 +1,21 @@
+package com.bussorenre.s99.lists
+
+import com.bussorenre.s99.AbstractProblemSpec
+
+class P16Spec extends AbstractProblemSpec{
+
+  val sut = P16
+
+  "drop" should {
+    "Drop every Nth element from a list." in {
+      sut.drop(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) must be(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    }
+  }
+
+  "drop2" should {
+    "Drop every Nth element from a list." in {
+      sut.drop2(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) must be(List('a, 'b, 'd, 'e, 'g, 'h, 'j, 'k))
+    }
+  }
+
+}
