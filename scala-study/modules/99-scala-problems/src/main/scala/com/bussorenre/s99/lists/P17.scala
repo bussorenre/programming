62bf566127717bc15d623f3ab6303119e1784c45
Ryo Matsumoto <ryo.mats302@gmail.com>
2019-10-04 03:36:31 +0900

    [s99] Problem 17 split

--- scala-study/modules/99-scala-problems/src/main/scala/com/bussorenre/s99/lists/P17.scala
+++ scala-study/modules/99-scala-problems/src/main/scala/com/bussorenre/s99/lists/P17.scala
@@ -1,5 +1,15 @@
 package com.bussorenre.s99.lists
 
+/**
+  * Split a list into two parts.
+  */
 object P17 {
-
+  def split[_](n: Int, list: List[_]): (List[_], List[_]) = (n, list) match {
+    case (_, Nil) => (Nil, Nil)
+    case (0, _)   => (Nil, list)
+    case (n, x :: xs) => {
+      val (prev, next) = split(n - 1, xs)
+      (x :: prev, next)
+    }
+  }
 }
--- scala-study/modules/99-scala-problems/src/test/scala/com/bussorenre/s99/lists/P17Spec.scala
+++ scala-study/modules/99-scala-problems/src/test/scala/com/bussorenre/s99/lists/P17Spec.scala
@@ -1,5 +1,14 @@
 package com.bussorenre.s99.lists
 
-class P17Spec {
+import com.bussorenre.s99.AbstractProblemSpec
 
+class P17Spec extends AbstractProblemSpec {
+  val sut = P17
+
+  "split" should {
+    "Split a list into two parts." in {
+      val expected = (List('a, 'b, 'c), List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+      sut.split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k)) must be(expected)
+    }
+  }
 }
